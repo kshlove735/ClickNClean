003c2cc58a5c18841ed1d2f381923e90aca16721
SeungHyunKim(big) <kshlove735@gmail.com>
2021-12-24 04:53:07 +0900

    주석 수정

--- src/kr/or/iei/admin/model/service/AdminService.java
+++ src/kr/or/iei/admin/model/service/AdminService.java
@@ -4,6 +4,12 @@ import kr.or.iei.admin.model.vo.Admin;
 
 public interface AdminService {
 
+	/**
+	 * @Author : SeungHyun Kim(kshlove735)
+	 * @Date : 2021. 12. 24.
+	 * @return : Admin
+	 * @subscription : 관리자 로그인 
+	 */
 	public Admin selectOneUser(String adminId, String adminPwd);
 
 }
