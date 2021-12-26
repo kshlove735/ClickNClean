6712356e972832eb40ec450f545e9eb0c4fb2074
jieun <kkbok0101@gmail.com>
2021-12-27 00:30:38 +0900

    dd

--- src/kr/or/iei/admin/model/service/AdminService.java
+++ src/kr/or/iei/admin/model/service/AdminService.java
@@ -1,5 +1,9 @@
 package kr.or.iei.admin.model.service;
 
+import kr.or.iei.admin.model.vo.Admin;
+
 public interface AdminService {
+
+	Admin selectOneUser(String adminId, String adminPwd);
 
 }
--- src/kr/or/iei/admin/model/service/AdminServiceImpl.java
+++ src/kr/or/iei/admin/model/service/AdminServiceImpl.java
@@ -1,10 +1,21 @@
 package kr.or.iei.admin.model.service;
 
+import java.sql.Connection;
+
 import kr.or.iei.admin.model.dao.AdminDAO;
+import kr.or.iei.admin.model.vo.Admin;
+import kr.or.iei.common.JDBCTemplate;
 
-public class AdminServiceImpl implements AdminService{
+public class AdminServiceImpl implements AdminService {
 
-	private AdminDAO adDAO = new AdminDAO();
-	
-	
+   private AdminDAO adDAO= new AdminDAO();
+   
+   @Override
+   public Admin selectOneUser(String adminId, String adminPwd) {
+      Connection conn = JDBCTemplate.getConnection();
+      Admin ad =adDAO.selectOneUser(conn, adminId, adminPwd);
+      JDBCTemplate.close(conn);
+      return ad;
+   }
+
 }
