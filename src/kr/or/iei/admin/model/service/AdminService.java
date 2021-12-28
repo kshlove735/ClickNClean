eaf9c6bf8b5b3527728f066f0680750a65b7fe5f
JDS <entlrsla@gmail.com>
2021-12-29 06:31:18 +0900

    삭제

--- src/kr/or/iei/admin/model/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package kr.or.iei.admin.model.service;
-
-import kr.or.iei.admin.model.vo.Admin;
-
-public interface AdminService {
-
-	Admin selectOneUser(String adminId, String adminPwd);
-
-}
--- src/kr/or/iei/review/model/service/ReviewService.java
+++
@@ -1,9 +0,0 @@
-package kr.or.iei.review.model.service;
-
-import java.util.HashMap;
-
-public interface ReviewService {
-
-	HashMap<String, Object> ReviewAllList(String coId, int currentPage);
-
-}
